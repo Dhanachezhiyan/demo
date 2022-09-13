f379a6edcb9d78ed9cd3d16ddbe1fe21b4238cf5
“Sultan15” <tgblite1509@gmail.com>
2022-09-13 05:05:25 +0000

    2nd

--- code.py
+++ code.py
@@ -5,7 +5,7 @@ if num > 0:
     print(num, "is a positive number.")
 print("This is always printed.")
 
-num = -1
+num = -2
 if num > 0:
     print(num, "is a positive number.")
 print("This is also always printed.")
